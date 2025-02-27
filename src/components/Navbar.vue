7ce2baba87ef658ebabc59b114a2318b95cd5b3c
Conversx <jdxisuku0009@gmail.com>
2025-02-27 14:14:13 +0700

    edit navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,45 +1,45 @@
 <template>
-    <v-app>
-      <v-navigation-drawer v-model="drawer" app>
-        <v-list dense>
-          <v-list-item-group>
-            <v-list-item v-for="(item, index) in menuItems" :key="index" :to="item.to">
-              <v-list-item-icon>
-                <v-icon>{{ item.icon }}</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>
-                <v-list-item-title>{{ item.text }}</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
-      </v-navigation-drawer>
-      <v-app-bar app color="primary" dark>
-        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-toolbar-title>test</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn icon>
-          <v-icon>mdi-account</v-icon>
-        </v-btn>
-      </v-app-bar>
-      <v-main>
-        <v-container>
-          <router-view></router-view>
-        </v-container>
-      </v-main>
-    </v-app>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        drawer: false,
-        menuItems: [
-          { text: "หน้าแรก", icon: "mdi-home", to: "/" },
-        ],
-      };
-    },
-  };
-  </script>
-  
+  <v-app>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item-group>
+          <v-list-item v-for="(item, index) in menuItems" :key="index" :to="item.to">
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.text }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>test</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main style="background-color: #e0e0e0;">
+      <v-container
+        style="background-image: url('/path/to/your/image.jpg'); background-size: cover; background-position: center;">
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      menuItems: [
+        { text: "หน้าแรก", icon: "mdi-home", to: "/" },
+      ],
+    };
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Test from '@/views/test.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: '/', component: Home },
+  { path: '/about', component: Test },
 ];
 
 const router = new VueRouter({
