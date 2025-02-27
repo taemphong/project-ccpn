f42cc36e551039312d7bfbe82a88953a924cfaff
Boatts <boatseed159@gmail.com>
2025-02-27 13:56:11 +0700

    commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-app>
+      <v-navigation-drawer v-model="drawer" app>
+        <v-list dense>
+          <v-list-item-group>
+            <v-list-item v-for="(item, index) in menuItems" :key="index" :to="item.to">
+              <v-list-item-icon>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>{{ item.text }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+      <v-app-bar app color="primary" dark>
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title>test</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>mdi-account</v-icon>
+        </v-btn>
+      </v-app-bar>
+      <v-main>
+        <v-container>
+          <router-view></router-view>
+        </v-container>
+      </v-main>
+    </v-app>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        drawer: false,
+        menuItems: [
+          { text: "หน้าแรก", icon: "mdi-home", to: "/" },
+        ],
+      };
+    },
+  };
+  </script>
+  
---
+++ src/views/Home.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>asdasd</h1>
+</template>
