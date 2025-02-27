6aaca3bebbfe7860fd15da1db4ea1fc52a494c32
Conversx <jdxisuku0009@gmail.com>
2025-02-27 16:21:29 +0700

    edit UserLoginData page

--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@ import App from './App.vue';
 import router from './router'; 
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
 
 Vue.use(Vuetify);
 
