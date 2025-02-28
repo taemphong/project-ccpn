9df6c7dcf37b3271f98d744a15b8fd291b905bf7
Conversx <jdxisuku0009@gmail.com>
2025-02-28 16:33:04 +0700

    test commit

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,42 +1,36 @@
 <template>
-<<<<<<< HEAD
-    <div>Login Page</div>
-</template>
-=======
-    <div class="login-container">
-      <div class="left-side">
-        <img src="~@/assets/images/logo123.png" alt="Logo" class="logo" />
-      </div>
-      <div class="right-side">Login Page</div>
+  <div class="login-container">
+    <div class="left-side">
+      <img src="~@/assets/images/logo123.png" alt="Logo" class="logo" />
     </div>
-  </template>
-  
-  <style scoped>
-  .login-container {
-    display: flex;
-    height: 100vh;
-  }
-  
-  .left-side {
-    flex: 2; 
-    background-image: url('~@/assets/images/bg3.png'); 
-    background-size: cover;
-    background-position: center;
-  }
-  
-  .right-side {
-    flex: 1; 
-    background-color: white; 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+    <div class="right-side">Login Page</div>
+  </div>
+</template>
 
-  .logo {
-  width: 120px; 
+<style scoped>
+.login-container {
+  display: flex;
+  height: 100vh;
+}
+
+.left-side {
+  flex: 2;
+  background-image: url('~@/assets/images/bg3.png');
+  background-size: cover;
+  background-position: center;
+}
+
+.right-side {
+  flex: 1;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  width: 120px;
   margin-top: 20px;
   margin-right: 20px;
-  }
-  </style>
-  
->>>>>>> 5117811392e2a4fbf0a4f607e88aa32e973a75e5
+}
+</style>
