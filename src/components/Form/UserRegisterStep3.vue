60e012f979f46ae935589a53920e491390c8fad9
Boatts <boatseed159@gmail.com>
2025-03-04 15:34:47 +0700

    register3

--- src/components/Form/UserRegisterStep3.vue
+++ src/components/Form/UserRegisterStep3.vue
@@ -1,1 +1,14 @@
-
+<template>
+    <v-container class="d-flex flex-column align-center justify-center" style="min-height: 100vh;">
+      <h2 class="text-center mb-4">เอกสารการสมัครของท่านได้รับส่งเข้าสู่ระบบเรียบร้อยแล้ว</h2>
+      <v-card class="pa-4" max-width="500">
+        <v-card-text>
+          นรก
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </template>
+  
+  <script setup>
+  </script>
+  
