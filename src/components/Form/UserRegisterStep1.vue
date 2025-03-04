a23942682ceee14283679df34df2224e88e29fcc
WaveFerG <waveziizaakub@gmail.com>
2025-03-04 15:04:22 +0700

    UserRegis

---
+++ src/components/Form/UserRegisterStep1.vue
@@ -0,0 +1,35 @@
+
+// UserRegisterStep1.vue
+<template>
+  <v-card class="pa-4">
+    <v-card-title>ข้อมูลส่วนตัว</v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-select
+          v-model="prefix"
+          :items="prefixOptions"
+          label="คำนำหน้าชื่อ"
+          outlined
+        ></v-select>
+        <v-text-field v-model="firstName" label="ชื่อ" outlined></v-text-field>
+        <v-text-field v-model="lastName" label="นามสกุล" outlined></v-text-field>
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      prefix: '',
+      firstName: '',
+      lastName: '',
+      prefixOptions: ['นาย', 'นาง', 'นางสาว'],
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/User/UserRegister.vue
+++ src/views/User/UserRegister.vue
@@ -1,6 +1,5 @@
 <template>
     <v-container fluid class="pa-0">
-
         <div>
             <v-img
                 :src="require('@/assets/images/bg3.jpg')"
@@ -10,103 +9,65 @@
                 max-width="auto"
             />
         </div>
-        <v-stepper v-model="e1">
-    <v-stepper-header>
-      <v-stepper-step
-        :complete="e1 > 1"
-        step="1"
-      >
-        Name of step 1
-      </v-stepper-step>
+        <v-stepper v-model="e1" alt-labels>
+            <v-stepper-header>
+                <v-stepper-step :complete="e1 > 1" step="1">
+                    Name of step 1
+                </v-stepper-step>
 
-      <v-divider></v-divider>
+                <v-divider></v-divider>
 
-      <v-stepper-step
-        :complete="e1 > 2"
-        step="2"
-      >
-        Name of step 2
-      </v-stepper-step>
+                <v-stepper-step :complete="e1 > 2" step="2">
+                    Name of step 2
+                </v-stepper-step>
 
-      <v-divider></v-divider>
+                <v-divider></v-divider>
 
-      <v-stepper-step step="3">
-        Name of step 3
-      </v-stepper-step>
-    </v-stepper-header>
+                <v-stepper-step step="3">
+                    Name of step 3
+                </v-stepper-step>
+            </v-stepper-header>
 
-    <v-stepper-items>
-      <v-stepper-content step="1">
-        <v-card
-          class="mb-12"
-          color="grey lighten-1"
-          height="200px"
-        ></v-card>
+            <v-stepper-items>
+                <v-stepper-content step="1">
+                    <UserRegisterStep1 />
+                    <v-btn color="primary" @click="e1 = 2"> Continue </v-btn>
+                </v-stepper-content>
 
-        <v-btn
-          color="primary"
-          @click="e1 = 2"
-        >
-          Continue
-        </v-btn>
+                <v-stepper-content step="2">
+                    <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
 
-        <v-btn text>
-          Cancel
-        </v-btn>
-      </v-stepper-content>
+                    <v-btn color="primary" @click="e1 = 3"> Continue </v-btn>
 
-      <v-stepper-content step="2">
-        <v-card
-          class="mb-12"
-          color="grey lighten-1"
-          height="200px"
-        ></v-card>
+                    <v-btn v-if="e1 > 1" text @click="e1--"> ย้อนกลับ </v-btn>
+                </v-stepper-content>
 
-        <v-btn
-          color="primary"
-          @click="e1 = 3"
-        >
-          Continue
-        </v-btn>
+                <v-stepper-content step="3">
+                    <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
 
-        <v-btn text>
-          Cancel
-        </v-btn>
-      </v-stepper-content>
+                    <v-btn color="primary" @click="e1 = 1"> Continue </v-btn>
 
-      <v-stepper-content step="3">
-        <v-card
-          class="mb-12"
-          color="grey lighten-1"
-          height="200px"
-        ></v-card>
-
-        <v-btn
-          color="primary"
-          @click="e1 = 1"
-        >
-          Continue
-        </v-btn>
-
-        <v-btn text>
-          Cancel
-        </v-btn>
-      </v-stepper-content>
-    </v-stepper-items>
-  </v-stepper>
+                    <v-btn v-if="e1 > 1" text @click="e1--"> ย้อนกลับ </v-btn>
+                </v-stepper-content>
+            </v-stepper-items>
+        </v-stepper>
     </v-container>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        e1: 1,
-      }
+import UserRegisterStep1 from "@/components/Form/UserRegisterStep1.vue";
+
+export default {
+    components: {
+        UserRegisterStep1,
     },
-  }
+    data() {
+        return {
+            e1: 1,
+        };
+    },
+};
 </script>
 
 <style>
-
 </style>
