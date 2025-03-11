886a0daf04507b1e94c4650fe950bc8a96875cb6
WaveFerG <waveziizaakub@gmail.com>
2025-03-11 14:32:34 +0700

    step1

--- src/components/Form/UserRegisterStep1.vue
+++ src/components/Form/UserRegisterStep1.vue
@@ -77,20 +77,22 @@
         <v-text-field v-model="mp_email" outlined></v-text-field>
       </v-col>
     </v-row>
-    <v-row class="mt-4 ml-1 required">
+    <v-row class="ml-1 ">
+      <div class="required">
       วัน-เดือน-ปีเกิด
-    </v-row>
+      </div>
+</v-row>
     <v-row class="v-raw-between">
       <v-col cols="12" md="3" class="custom-col">
-        <div class="mb-3 required">วัน</div>
+        <div class="mb-3 required">วัน </div>
         <v-select v-model="mp_birthday" :items="dayOptions" outlined></v-select>
       </v-col>
       <v-col cols="12" md="3" class="custom-col">
-        <div class="mb-3 required">เดือน</div>
+        <div class="mb-3 required">เดือน </div>
         <v-select v-model="mp_birthmonth" :items="monthOptions" outlined></v-select>
       </v-col>
       <v-col cols="12" md="3" class="custom-col margin-right">
-        <div class="mb-3 required">ปี</div>
+        <div class="mb-3 required">ปี </div>
         <v-select v-model="mp_birthyear" :items="yearOptions" outlined></v-select>
       </v-col>
       <v-col cols="12" md="3">
@@ -99,21 +101,23 @@
       </v-col>
     </v-row>
 
-    <v-row class="mt-4 ml-1 required">
-      วันหมดอายุบัตร
+    <v-row class="ml-1 ">
+      <div class="required">
+      วันหมดอายุบัตร 
+    </div>
     </v-row>
     <v-row class="v-raw-between">
       <v-col cols="12" md="3" class="custom-col">
-        <div class="mb-3 required">วัน</div>
-        <v-select v-model="expireDay" :items="dayOptions" outlined></v-select>
+        <div class="mb-3 required">วัน </div>
+        <v-select v-model="ml_expired" :items="dayOptions" outlined></v-select>
       </v-col>
       <v-col cols="12" md="3" class="custom-col">
-        <div class="mb-3 required">เดือน</div>
-        <v-select v-model="expireMonth" :items="monthOptions" outlined></v-select>
+        <div class="mb-3 required">เดือน </div>
+        <v-select v-model="ml_expired" :items="monthOptions" outlined></v-select>
       </v-col>
       <v-col cols="12" md="3" class="custom-col margin-right">
-        <div class="mb-3 required">ปี</div>
-        <v-select v-model="expireYear" :items="yearOptions2" outlined></v-select>
+        <div class="mb-3 required">ปี </div>
+        <v-select v-model="ml_expired" :items="yearOptions2" outlined></v-select>
       </v-col>
       <v-col cols="12" md="3">
         <div class="mb-3">ออกโดย</div>
@@ -471,9 +475,7 @@ export default {
         "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม"
       ],
       yearOptions: Array.from({ length: 100 }, (_, i) => new Date().getFullYear() - i),
-      expireDay: '',
-      expireMonth: '',
-      expireYear: '',
+      ml_expired: '',
       mp_issued_by: '',
       yearOptions2: Array.from({ length: 50 }, (_, i) => new Date().getFullYear() + i),
 
